378c9b45501e876ec0aa52ec1636f49d2a6d10fd
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-04-30 15:06:27 -0300

    add: begin home page

--- frontend/endo-front/src/pages/Home/index.js
+++ frontend/endo-front/src/pages/Home/index.js
@@ -8,7 +8,18 @@ function Home() {
             <div className={styles.container}>
                 <Header />
                 <div className={styles.content}>
-                    
+                    <div className={styles.salute}>
+                        <h1>Bem vindo!</h1>
+                        <div className={styles.typewriter}>EndoAI</div>
+                        <div className={styles.buttons}>
+                            <div className={styles.selectionButton}>
+                                Avaliação estendida
+                            </div>
+                            <div className={styles.selectionButton}>
+                                Avaliação simplificada
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
