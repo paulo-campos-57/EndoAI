cc0a81cd1ef195e68a5f79b308a0ab5095cf81f4
Diogo Henrique <116087739+DiogoHMC@users.noreply.github.com>
2025-05-02 16:48:26 -0300

    Adept: Fix project structure

--- backend/app/app.py
+++ backend/app/app.py
@@ -4,14 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Carrega o modelo e o scaler
-knn_model = joblib.load("knn_model.joblib")
-scaler = joblib.load("scaler.joblib")
 @app.route('/')
 def home():
     return render_template('index.html')  # Vai renderizar o HTML na pasta templates
-@app.route("/predict", methods=["POST"])
-def predict():
-        return 0
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/joblibVisu.py
@@ -0,0 +1,8 @@
+# import pickle
+
+# with open('scaler.joblib', 'rb') as f:
+#     modelo = pickle.load(f)
+
+# print("Tipo do objeto:", type(modelo))
+# print("Conteúdo:")
+# print(modelo)
