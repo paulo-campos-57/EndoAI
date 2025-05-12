457192be60cefc05dc2e634f2b402d7504ef234a
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-05-12 14:21:05 -0300

    style: change site font
    Co-authored-by: Estela Lacerda <elo@cesar.school>

--- frontend/endo-front/src/components/Header/index.js
+++ frontend/endo-front/src/components/Header/index.js
@@ -6,16 +6,18 @@ function Header() {
     return (
         <>
             <div className={styles.head}>
-                <div className={styles.leftSide}>
-                    <img className={styles.logo} src='/images/logo.png' alt='Logo do site' />
-                    <h2 className={styles.title}>
-                        EndoAI
-                    </h2>
-                </div>
-                <div className={styles.rightSide}>
-                    <Link to='/' className={styles.links}>Início</Link>
-                    <Link to='/sobre' className={styles.links}>Sobre</Link>
-                    <Link to='/extendido' className={styles.links}>EndoAI</Link>
+                <div className={styles.headContent}>
+                    <div className={styles.leftSide}>
+                        <img className={styles.logo} src='/images/logo.png' alt='Logo do site' />
+                        <h2 className={styles.title}>
+                            EndoAI
+                        </h2>
+                    </div>
+                    <div className={styles.rightSide}>
+                        <Link to='/' className={styles.links}>Início</Link>
+                        <Link to='/sobre' className={styles.links}>Sobre</Link>
+                        <Link to='/extendido' className={styles.links}>EndoAI</Link>
+                    </div>
                 </div>
             </div>
         </>
