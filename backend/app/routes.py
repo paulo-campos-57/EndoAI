8b7cece96b1e6376941235357c2a66c720d940bf
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2025-05-14 14:33:48 -0300

    add: insertt new features in About page

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, jsonify, request  
-import joblib 
-import pandas as pd  
+from flask import Blueprint, jsonify, request
+import joblib
+import pandas as pd
+from pathlib import Path
+
 routes_bp = Blueprint("routes", __name__)
+
+# Diretório atual do arquivo routes.py
+BASE_DIR = Path(__file__).resolve().parent
+
+# Caminhos absolutos para os arquivos
+model_path = BASE_DIR / "knn_model.joblib"
+scaler_path = BASE_DIR / "scaler.joblib"
+
+# Carregar o modelo KNN e o scaler
+knn_model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 
 
 @routes_bp.route("/api/salute", methods=["GET"])
@@ -9,41 +22,34 @@ def salute():
     return jsonify({"message": "Integrado com sucesso"})
 
 
-
-# Carregar o modelo KNN e o scaler
-knn_model = joblib.load('knn_model.joblib')  # Modelo KNN
-scaler = joblib.load('scaler.joblib')  # Scaler
-
-# Definindo a rota para prever a probabilidade de diabetes
-@routes_bp.route('/prever', methods=['POST'])
+@routes_bp.route("/prever", methods=["POST"])
 def prever_diabetes():
-    # Recebe os dados do POST
     dados = request.json
-    
-    # Converter os dados para DataFrame
     df_input = pd.DataFrame([dados])
-    
-    # Definir as features (colunas de entrada para o modelo)
-    features = ['HighBP', 'HighChol', 'BMI', 'Smoker', 'PhysActivity', 'Fruits',
-                'GenHlth', 'MentHlth', 'PhysHlth', 'Age', 'Education', 'Income']
+
+    features = [
+        "HighBP",
+        "HighChol",
+        "BMI",
+        "Smoker",
+        "PhysActivity",
+        "Fruits",
+        "GenHlth",
+        "MentHlth",
+        "PhysHlth",
+        "Age",
+        "Education",
+        "Income",
+    ]
     df_input = df_input[features]
-    
-    # Escalonar os dados
+
     df_input_scaled = scaler.transform(df_input)
-    
-    # Prever as probabilidades para as classes
     probabilidades = knn_model.predict_proba(df_input_scaled)
-    
-    # Obter a probabilidade de diabetes (classe 2)
     probabilidade_diabetes = round(probabilidades[0][2] * 100, 1)
-    
-    # Limitar a probabilidade a 95% caso seja maior que isso
+
     if probabilidade_diabetes > 95:
         probabilidade_diabetes = 95
-    
-    # Retornar a resposta com a chance de diabetes
-    resultado = {
-        "chance_diabetes": f"{probabilidade_diabetes}%"
-    }
-    
+
+    resultado = {"chance_diabetes": f"{probabilidade_diabetes}%"}
+
     return jsonify(resultado)
--- frontend/endo-front/src/pages/About/index.js
+++ frontend/endo-front/src/pages/About/index.js
@@ -14,13 +14,13 @@ function About() {
                         <div className={styles.block}>
                             <div className={`${styles.typewriterBase} ${styles.typewriter1}`}>Quem Somos</div>
                             <p className={styles.fontText}>
-                                Na luta contra a diabetes, informação é poder — e a Endo.AI está aqui para empoderar. Criada por estudantes do 6º período de Ciência da Computação do CESAR School, nossa solução usa inteligência artificial e vai além dos dados: ela educa e conscientiza sobre uma das condições mais silenciosas e perigosas da nossa sociedade.
+                                Na luta contra a diabetes, informação é poder — e a Endo.AI está aqui para empoderar. Criada por <a href="https://github.com/paulo-campos-57/EndoAI" className={styles.links}>estudantes do 6º período de Ciência da Computação do CESAR School</a>, nossa solução usa inteligência artificial e vai além dos dados: ela educa e conscientiza sobre uma das condições mais silenciosas e perigosas da nossa sociedade.
                             </p>
                         </div>
                         <div className={styles.block}>
                             <div className={`${styles.typewriterBase} ${styles.typewriter2}`}>Nossa Solução</div>
                             <p className={styles.fontText}>
-                                O Endo.AI é uma ferramenta inteligente, acessível e preventiva criada para ajudar na identificação precoce de possíveis casos de diabetes. Com base em respostas simples feitas em um questionário, nosso modelo de machine learning analisa padrões e estima a probabilidade de risco — tudo isso a partir de uma base de dados sólida e criteriosamente treinada.
+                                O <a href="https://sites.google.com/cesar.school/endo-ai/home" className={styles.links}>Endo.AI</a> é uma ferramenta inteligente, acessível e preventiva criada para ajudar na identificação precoce de possíveis casos de diabetes. Com base em respostas simples feitas em um questionário, nosso modelo de machine learning analisa padrões e estima a probabilidade de risco — tudo isso a partir de uma base de dados sólida e criteriosamente treinada.
                             </p>
                         </div>
                         <div className={styles.block}>
