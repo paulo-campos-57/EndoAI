c9894d85b885c534b842f66fea417f9078279325
unknown <mga@cesar.school>
2025-05-16 17:15:27 -0300

    feat: our team photos and information on the about section.

--- frontend/endo-front/src/pages/About/index.js
+++ frontend/endo-front/src/pages/About/index.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import styles from "./About.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 function About() {
+
+        const teamMembers = [
+        { 
+            image: '/images/Arthur-Paixao.png',
+            name: 'Arthur Paixão',
+            email: 'aptm@cesar.school'
+        },
+        { 
+            image: '/images/Diogo-Henrique.png',
+            name: 'Diogo Henrique',
+            email: 'dhmc@cesar.school'
+        },
+        { 
+            image: '/images/Estela-Lacerda.png',
+            name: 'Estela de Lacerda',
+            email: 'elo@cesar.school'
+        },
+        { 
+            image: '/images/Gabriel-Simoes.png',
+            name: 'Gabriel Simões',
+            email: 'gsr@cesar.school'
+        },
+        { 
+            image: '/images/Matheus-Gomes.png',
+            name: 'Matheus Gomes',
+            email: 'mga@cesar.school'
+        },
+        { 
+            image: '/images/Matheus-Bione.png',
+            name: 'Matheus Bione',
+            email: 'mvbb@cesar.school'
+        },
+        { 
+            image: '/images/Paulo-Campos.png',
+            name: 'Paulo Campos',
+            email: 'pmc3@cesar.school'
+        },
+    ];
+
     return (
         <>
             <div className={styles.container}>
@@ -28,6 +72,36 @@ function About() {
                             <p className={styles.fontText}>
                                 Sabemos que 1 em cada 3 pessoas com diabetes não sabe que têm a condição. O Endo.AI convida você a se conhecer melhor. A partir do resultado, o próximo passo pode ser procurar um profissional de saúde, fazer exames ou adotar hábitos mais saudáveis. É um gesto simples — mas pode ser o começo de uma grande transformação.
                             </p>
+                        </div>
+                        <div className={styles.block}>
+                            <div className={`${styles.typewriterBase} ${styles.typewriter4}`}>Nossa Equipe</div>
+                            <Swiper
+                                modules={[Autoplay, Navigation]}
+                                spaceBetween={30}
+                                slidesPerView={1}
+                                autoplay={{
+                                    delay: 5000,
+                                    disableOnInteraction: false,
+                                }}
+                                navigation={true}
+                                className={styles.teamCarousel}
+                            >
+                                {teamMembers.map((member, index) => (
+                                    <SwiperSlide key={index}>
+                                        <div className={styles.singleMemberContainer}>
+                                            <img 
+                                                src={member.image} 
+                                                alt={member.name}
+                                                className={styles.singleMemberImage} 
+                                            />
+                                            <div className={styles.memberInfo}>
+                                                <p className={styles.memberName}>{member.name}</p>
+                                                <p>{member.email}</p>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
                         </div>
                     </div>
                 </div>
